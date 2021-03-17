d954d95493a885c32dc59605841ea6817b5bbde9
Replit user <replituser@example.com>
2021-03-17 05:56:22 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -109,7 +109,8 @@ Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
-   return 0;
+    
+   return v[i].capacidad;
 }
 
 /*
